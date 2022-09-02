4ecd97b393a7e3edb2bd1406722617219858c57f
Matias Martinez <tinezmatias@gmail.com>
2022-09-02 09:35:18 -0300

    added comments and docs

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # POC of Codegen Graphql and Vendure
 
-This is just a demo project.
+How does it work?
+
+if you have a vendure server up and running on localhost on port 8000, this script will download the instrospection, convert it into a graphql schema, then generate the documents / definitions and as a result in the generated folder we will have the operations for the frontend.
+
+## Commands
+
+- yarn ( install dependencies )
+- yarn dev ( run the code generator )
--- src/codegen/index.ts
+++ src/codegen/index.ts
@@ -1,8 +1,8 @@
-import fs from "fs";
-import { generate } from "@graphql-codegen/cli";
+import fs from 'fs';
+import { generate } from '@graphql-codegen/cli';
 
-import * as constants from "./constants";
-import { getIntrospection, getPath, getSdlFromIntrospection } from "./utils";
+import * as constants from './constants';
+import { getIntrospection, getPath, getSdlFromIntrospection } from './utils';
 
 // Main manager of code generation
 export const main = async () => {
@@ -11,14 +11,14 @@ export const main = async () => {
     const { adminSchemaJson, shopSchemaJson } = await getIntrospection();
 
     // Conver jscon schema to sdl schema and save in file
-    getSdlFromIntrospection(adminSchemaJson.data, "admin-schema");
-    getSdlFromIntrospection(shopSchemaJson.data, "shop-schema");
+    getSdlFromIntrospection(adminSchemaJson.data, 'admin-schema');
+    getSdlFromIntrospection(shopSchemaJson.data, 'shop-schema');
 
     // Generate operation from schema / typedefs
     const plugins = [
-      "typescript",
-      "typescript-operations",
-      "typescript-react-apollo",
+      'typescript',
+      'typescript-operations',
+      'typescript-react-apollo',
     ];
 
     const config = {
@@ -31,15 +31,23 @@ export const main = async () => {
     await generate({
       overwrite: true,
       generates: {
+        // destination file
         [constants.ADMIN_OPS_OUTPUT_FILE]: {
+          // load schemas, the client schema is the same as in vendure
           schema: [constants.ADMIN_SCHEMA_OUTPUT_SDL, constants.CLIENT_SCHEMA],
           documents: [
             {
+              // generate the documents for the schema that was generated from the api
               [constants.ADMIN_SCHEMA_OUTPUT_SDL]: {
                 loader: constants.DEFINITIONS_LOADER,
               },
             },
             {
+              /**
+               * generate the documents for the client schema
+               * (here we pass the same schema as the previous one, but it is only a workaround since
+               *  the loader does not use it, it loads the client schema).
+               */
               [constants.ADMIN_SCHEMA_OUTPUT_SDL]: {
                 loader: constants.CLIENT_DEFINITIONS_LOADER,
               },
@@ -48,10 +56,13 @@ export const main = async () => {
           plugins,
           config,
         },
+        // destination file
         [constants.SHOP_OPS_OUTPUT_FILE]: {
+          // load the generated schema
           schema: [constants.SHOP_SCHEMA_OUTPUT_SDL],
           documents: [
             {
+              // generate the documents fot the schop schema
               [constants.SHOP_SCHEMA_OUTPUT_SDL]: {
                 loader: constants.DEFINITIONS_LOADER,
               },
@@ -65,6 +76,6 @@ export const main = async () => {
 
     return;
   } catch (error) {
-    fs.writeFileSync(getPath("errors.txt"), `${error}`);
+    fs.writeFileSync(getPath('errors.txt'), `${error}`);
   }
 };
